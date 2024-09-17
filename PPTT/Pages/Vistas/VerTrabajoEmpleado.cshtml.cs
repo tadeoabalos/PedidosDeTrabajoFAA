1887d974870fafb887c65cde4c2cb792a941fece
FFattori24 <136878766+FFattori24@users.noreply.github.com>
2024-09-17 09:52:13 -0300

    Agregar Botones y fixes
    "
    
    Co-Authored-By: Tadeo Abalos <128402901+tadeoabalos@users.noreply.github.com>

---
+++ PPTT/Pages/Vistas/VerTrabajoEmpleado.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PPTT.Pages.Vistas
+{
+    public class VerTrabajoEmpleadoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
