a5ebbc2cdaa7964666cb555f7afc998901541bb5
FFattori24 <136878766+FFattori24@users.noreply.github.com>
2024-10-24 10:09:43 -0300

    "
    "

--- PPTT/Pages/PT/Details.cshtml.cs
+++ PPTT/Pages/PT/Details.cshtml.cs
@@ -19,16 +19,18 @@ namespace PPTT.Pages.PT
         {
             _context = context;
         }
+
         public List<Admin> Usuarios { get; set; } = new List<Admin>();
         public PTUsuario? PedidoTrabajo { get; set; } = default!;
-        public string motivo { get; set; }
         public List<Estado> Estado { get; set; } = default!;
         public List<Prioridad> Prioridad { get; set; } = new List<Prioridad>();
+
         public async Task<JsonResult> OnGetUsuariosFiltradosAsync(string division)
         {
             var usuarios = await _context.GetUsuariosFiltradosAsync(int.Parse(division));
             return new JsonResult(usuarios);
         }
+
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             Usuarios = await _context.GetUsuariosAsync();
@@ -40,24 +42,25 @@ namespace PPTT.Pages.PT
             Estado = await _context.GetEstadosAsync();
             Prioridad = await _context.GetPrioridadAsync();
             PedidoTrabajo = await _context.PTUsuario
-                .Include(pt => pt.Organismo) 
-                .Include(pt => pt.Tarea) 
-                .Include(pt => pt.Estado) 
-                .Include(pt => pt.Dependencia_Interna) 
+                .Include(pt => pt.Organismo)
+                .Include(pt => pt.Tarea)
+                .Include(pt => pt.Estado)
+                .Include(pt => pt.Dependencia_Interna)
                 .Include(pt => pt.Grado)
                 .Include(pt => pt.Prioridad)
-                .FirstOrDefaultAsync(m => m.ID_Orden_Trabajo_Pk == id); 
+                .FirstOrDefaultAsync(m => m.ID_Orden_Trabajo_Pk == id);
 
             if (PedidoTrabajo == null)
             {
                 return NotFound();
             }
-            // Guardar el valor de ID_Tarea_Fk en la sesión
+
+            // Guardar el valor de ID_Tarea_Fk y Correo en la sesión
             HttpContext.Session.SetInt32("ID_Tarea_Fk", PedidoTrabajo.ID_Tarea_Fk);
             HttpContext.Session.SetString("CorreoUsuario", PedidoTrabajo.Correo);
-            HttpContext.Session.SetString("motivo", motivo);
             return Page();
         }
+
         public async Task<JsonResult> OnGetPrioridadesAsync()
         {
             var prioridades = await _context.GetPrioridadAsync();
@@ -69,25 +72,37 @@ namespace PPTT.Pages.PT
             await _context.Database.ExecuteSqlRawAsync("EXEC [dbo].[FinalizarPedidoTrabajo] @p0", OrdenTrabajoId);
             return RedirectToPage("./Index");
         }
-        public async Task<IActionResult> OnPostSuspenderEstadoAsync(int OrdenTrabajoId, DateTime fechaEstimadaFin)
+
+        public async Task<IActionResult> OnPostSuspenderEstadoAsync(int OrdenTrabajoId, DateTime fechaEstimadaFin, string motivo)
         {
+            // Convertir la fecha a string
             string fechaComoString = fechaEstimadaFin.ToString("dd/MM/yyyy");
             HttpContext.Session.SetString("FechaEstimadaFin", fechaComoString);
+
+            // Validar y almacenar el motivo en la sesión
+            if (!string.IsNullOrEmpty(motivo))
+            {
+                HttpContext.Session.SetString("motivo", motivo);
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(motivo), "El motivo no puede ser nulo o vacío.");
+            }
+
             await _context.Database.ExecuteSqlRawAsync("EXEC [dbo].[SuspenderPedidoTrabajo] @p0, @p1", OrdenTrabajoId, fechaEstimadaFin);
             return RedirectToPage("./Index");
         }
+
         public async Task<IActionResult> OnPostCancelarEstadoAsync(int OrdenTrabajoId)
         {
             await _context.Database.ExecuteSqlRawAsync("EXEC [dbo].[CancelarPedidoTrabajo] @p0", OrdenTrabajoId);
             return RedirectToPage("./Index");
         }
+
         public async Task<IActionResult> OnPostPendienteEstadoAsync(int OrdenTrabajoId)
         {
             await _context.Database.ExecuteSqlRawAsync("EXEC [dbo].[PendientePedidoTrabajo] @p0", OrdenTrabajoId);
             return RedirectToPage("./Index");
         }
-        
-
     }
 }
-
