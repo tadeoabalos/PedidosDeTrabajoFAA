014e32f67430ae624469836f0ccc140ebc6bae90
FFattori24 <136878766+FFattori24@users.noreply.github.com>
2024-10-24 08:37:26 -0300

    "
    "

---
+++ PPTT/Pages/PT/MandarMailCambioEstado.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PPTT.Pages.PT
+{
+    public class MandarMailCambioEstadoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
