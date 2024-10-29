3cf6592c57c8df7743884ea5ee4c62dac7cd130d
Tadeo Abalos <128402901+tadeoabalos@users.noreply.github.com>
2024-10-29 09:13:26 -0300

    29/10

--- PPTT/Pages/PT/Index.cshtml.cs
+++ PPTT/Pages/PT/Index.cshtml.cs
@@ -18,24 +18,31 @@ namespace PPTT.Pages.Administradores
         {
             _context = context;
         }      
-        public IList<PTUsuario> PedidoTrabajo { get;set; } = default!;
+        //public IList<PTUsuario> PedidoTrabajo { get;set; } = default!;
+        public PaginatedList<PTUsuario> PedidoTrabajo { get; set; } = default!;
+
         [BindProperty]
         public List<Admin> Usuarios { get; set; } = new List<Admin>();
         public Orden_Asignada Orden_Asignada { get; set; }
         public PTUsuario PT { get; set; } = default!;
         public List<Prioridad> Prioridad { get; set; } = new List<Prioridad>();
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? pageIndex)
         {
+            int pageSize = 8; 
             Prioridad = await _context.GetPrioridadAsync();
-            PedidoTrabajo = await _context.PTUsuario
-                .Include(pt => pt.Organismo)           
+
+            var pedidosQuery = _context.PTUsuario
+                .Include(pt => pt.Organismo)
                 .Include(pt => pt.Tarea)
                 .Include(pt => pt.Estado)
                 .Include(pt => pt.Prioridad)
                 .Include(pt => pt.Dependencia_Interna)
                 .Include(pt => pt.Grado)
-                .ToListAsync();            
+                .AsQueryable();
+
+            PedidoTrabajo = await PaginatedList<PTUsuario>.CreateAsync(pedidosQuery, pageIndex ?? 1, pageSize);
         }
+
         public async Task<JsonResult> OnGetUsuariosFiltradosAsync(string division)
         {
             var usuarios = await _context.GetUsuariosFiltradosAsync(int.Parse(division));
@@ -63,4 +70,28 @@ namespace PPTT.Pages.Administradores
             return new JsonResult(prioridades);
         }       
     }
+
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
+
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+
 }
