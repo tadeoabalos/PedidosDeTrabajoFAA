4b562aa8600485479d7d5a284495a74508237b8f
Tadeo Abalos <128402901+tadeoabalos@users.noreply.github.com>
2024-11-12 08:05:50 -0300

    Commit 12/11

--- PPTT/Models/Admin.cs
+++ PPTT/Models/Admin.cs
@@ -7,15 +7,21 @@ namespace PPTT.Models
     {        
         public int ID_Usuario_Pk { get; set; }
         [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        [StringLength(50)]
         public string? Nombre { get; set; }
         [Display(Name = "Apellido")]
+        [Required(ErrorMessage = "El campo Apellido es obligatorio")]
+        [StringLength(50)]
         public string? Apellido { get; set; }
+        [Required(ErrorMessage = "El campo Correo es obligatorio")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@faa\.mil\.ar$", ErrorMessage = "Por favor, ingresa un correo institucional válido (@faa.mil.ar).")]
+        [StringLength(50)]
         [Display(Name = "Correo institucional")]
         public string? Correo { get; set; }
 
         public enum Rol
-        {
-            // Asegúrate de tener valores definidos aquí
+        {            
             Operario = 1,
             Administrador = 2,
             SuperAdministrador = 3
@@ -26,14 +32,17 @@ namespace PPTT.Models
         [Display(Name = "Contraseña")]
         public int? ID_Password_Fk { get; set; }
         [Display(Name = "Numero de documento")]
+        [Required(ErrorMessage = "El campo DNI es obligatorio")]        
+        [Range(1000000, 99999999, ErrorMessage = "Número inválido de DNI")]
         public int DNI { get; set; }
         [Display(Name = "Numero de control")]
+        [Required(ErrorMessage = "El campo número de control es obligatorio")]                
         public int? Numero_Control { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar una división.")]
         [Display(Name = "División")]
         public int? ID_Division_Fk {  get; set; }
         [Display(Name = "Servicio")]
-
-        [Required(ErrorMessage = "Debes seleccionar un servicio.")]
+        [Required(ErrorMessage = "Debe seleccionar un servicio.")]
         public int? ID_Servicio_Fk { get; set; }
         [Display(Name ="Fecha de Alta")]
         public DateTime? Fecha_Alta { get; set; }
--- PPTT/Models/PTUsuario.cs
+++ PPTT/Models/PTUsuario.cs
@@ -18,10 +18,12 @@ namespace PPTT.Models
 
         [Display(Name = "IP")]
         public string? IP_Solicitante { get; set; } = string.Empty;
-
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        [StringLength(50)]
         [Display(Name = "Nombre")]
         public string? Nombre_Solicitante { get; set; } = string.Empty;
-
+        [Required(ErrorMessage = "El campo Apellido es obligatorio")]
+        [StringLength(50)]
         [Display(Name = "Apellido")]
         public string? Apellido_Solicitante { get; set; } = string.Empty;
 
@@ -29,7 +31,9 @@ namespace PPTT.Models
         public int ID_Grado_Fk { get; set; }
         [ForeignKey("ID_Grado_Fk")]
         public Grado? Grado { get; set; }
-
+        [Required(ErrorMessage = "El campo Correo es obligatorio")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@faa\.mil\.ar$", ErrorMessage = "Por favor, ingresa un correo institucional válido (@faa.mil.ar).")]
+        [StringLength(50)]
         [Display(Name = "Correo")]
         public string? Correo { get; set; } = string.Empty;
 
@@ -37,25 +41,25 @@ namespace PPTT.Models
         public int ID_Organismo_Fk { get; set; }
         [ForeignKey("ID_Organismo_Fk")]
         public Organismo? Organismo { get; set; }
-
+        [Required(ErrorMessage = "El campo RTI es obligatorio")]
         [Display(Name = "RTI")]
         public string? RTI_Solicitante { get; set; } = string.Empty;
-
+        [Required(ErrorMessage = "El campo color del sector es obligatorio")]
         [Display(Name = "Color del sector")]
         public string? Color_Oficina_PT { get; set; } = string.Empty;
-
+        [Required(ErrorMessage = "El campo N° de oficina es obligatorio")]
         [Display(Name = "N° Oficina")]
         public string? Numero_Oficina_PT { get; set; } = string.Empty;
-
+        [Required(ErrorMessage = "El campo piso es obligatorio")]
         [Display(Name = "Piso de la Oficina")]
         public string? Piso_Oficina_PT { get; set; } = string.Empty;
 
         [Display(Name = "Color de la Oficina del Solicitante")]
         public string? Color_Oficina_Solicitante { get; set; } = string.Empty;
-
+        [Required(ErrorMessage = "El campo N° de oficina es obligatorio")]
         [Display(Name = "Número de Oficina del Solicitante")]
         public string? Numero_Oficina_Solicitante { get; set; } = string.Empty;
-
+        [Required(ErrorMessage = "El campo Piso es obligatorio")]
         [Display(Name = "Piso de la Oficina del Solicitante")]
         public string? Piso_Oficina_Solicitante { get; set; } = string.Empty;
 
@@ -63,9 +67,11 @@ namespace PPTT.Models
         public int ID_Dependencia_Interna_Fk { get; set; }
         [ForeignKey("ID_Dependencia_Interna_Fk")]
         public Dependencia_Interna? Dependencia_Interna { get; set; }
+        [Required(ErrorMessage = "El campo DNI es obligatorio")]
         [Display(Name = "DNI del Solicitante")]
-        public int DNI_Solicitante { get; set; } 
-
+        [Range(1000000, 99999999, ErrorMessage = "Número inválido de DNI")]
+        public int DNI_Solicitante { get; set; }
+        [Required(ErrorMessage = "El campo Número de control es obligatorio")]        
         [Display(Name = "Número de Control del Solicitante")]
         public string? Numero_Control_Solicitante { get; set; } = string.Empty;
         
@@ -95,6 +101,7 @@ namespace PPTT.Models
         public int ID_Division_Fk { get; set; }
         [ForeignKey("ID_Division_Fk")]
         public Division? Division { get; set; }
+        [StringLength(250)]
         [Display(Name = "Observaciones")]
         public string? Observacion { get; set; }
        
